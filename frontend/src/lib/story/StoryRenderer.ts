1b9315d30ebdb46cfcd6a0ee7cafe75486d4e696
Yoann Balasse <142342012+Nota-yob@users.noreply.github.com>
2026-01-04 01:28:13 +0100

    feat: timerange card and vscode productivity story

--- frontend/src/lib/story/StoryRenderer.ts
+++ frontend/src/lib/story/StoryRenderer.ts
@@ -1,11 +1,13 @@
 import HeroStat from '$lib/cards/HeroStat.svelte';
 import TopItem from '$lib/cards/TopItem.svelte';
 import TopNItems from '$lib/cards/TopNItems.svelte';
+import TimeRange from '$lib/cards/TimeRange.svelte';
 
 // later: TopItem, HighestLowest, etc.
 
 export const StoryRenderer: Record<string, any> = {
   HERO_STAT: HeroStat,
   TOP_ITEM: TopItem,
-  TOP_N_ITEMS: TopNItems
+  TOP_N_ITEMS: TopNItems,
+  TIME_RANGE: TimeRange
 };
